104744bf70b5c986ab6c5eb11b6225f7411db2fb
ahahau <ahahau1@naver.com>
2025-03-19 19:34:52 +0900

    1024

--- BackJooncpp/BackJooncpp/Test.cpp
+++ BackJooncpp/BackJooncpp/Test.cpp
@@ -1,7 +1,15 @@
-#include<iostream>
-using namespace std;
-
+//#include<iostream>
+//using namespace std;
 //int main()
 //{
-//	
+//	int a, b,c,d = 0;
+//	int length;
+//	int* array = new int[length];
+//	cin >> a >> b;
+//	for (int i = 0; i < length; i++)
+//	{
+//		cin >> array[i];
+//	}
+//	cout << d;
 //}
+
