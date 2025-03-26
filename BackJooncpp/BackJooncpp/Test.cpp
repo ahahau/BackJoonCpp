e4b62535f06906b687cf45876dc9cb8959953bd9
ahahau <ahahau1@naver.com>
2025-03-26 18:41:44 +0900

    1978

--- BackJooncpp/BackJooncpp/Test.cpp
+++ BackJooncpp/BackJooncpp/Test.cpp
@@ -1,18 +1,18 @@
-#include<iostream>
-using namespace std;
-int cal(int a, int b);
-int main()
-{
-	int a, b,r;
-	cin >> a >> b;
-	while (true)
-	{
-		if (b == 0)
-			break;
-		r = a / b;
-		a = b;
-		b = r;
-	}
-	cout << a;
-}
-
+//#include<iostream>
+//using namespace std;
+//int cal(int a, int b);
+//int main()
+//{
+//	int a, b,r;
+//	cin >> a >> b;
+//	while (true)
+//	{
+//		if (b == 0)
+//			break;
+//		r = a / b;
+//		a = b;
+//		b = r;
+//	}
+//	cout << a;
+//}
+//
