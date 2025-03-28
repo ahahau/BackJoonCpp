e933cafc0c48922d7ac10320ec3efe315f9ce575
ahahau <ahahau1@naver.com>
2025-03-28 14:32:48 +0900

    1181

---
+++ BackJooncpp/BackJooncpp/1181.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include <algorithm>
+using namespace std;
+bool Comp(string a, string b);
+int main()
+{
+    int n, tmp;
+    cin >> n;
+    string* arr = new string[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    sort(arr, arr + n, Comp);
+    string before = "";
+    for (int i = 0; i < n; i++) 
+    {
+        if (before == arr[i]) 
+            continue;
+        cout << arr[i] << endl;
+        before = arr[i];
+    }
+}
+bool Comp(string a, string b) {
+    if (a.length() != b.length()) 
+    {
+        return a.length() < b.length();
+    }
+    else 
+    {
+        return a < b;
+    }
+}
