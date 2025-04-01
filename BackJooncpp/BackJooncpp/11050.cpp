6afd695878fe168512f7837d06ea33005086f866
ahahau <ahahau1@naver.com>
2025-04-01 18:39:02 +0900

    1110

--- BackJooncpp/BackJooncpp/11050.cpp
+++ BackJooncpp/BackJooncpp/11050.cpp
@@ -1,21 +1,21 @@
-#include <iostream>
-using namespace std;
-
-int Factorial(int t) 
-{
-    int ans = 1;
-    for (int i = t; i > 0; i--) {
-        ans *= i;
-    }
-    return ans;
-}
-
-int main() 
-{
-    int n, k;
-    cin >> n >> k;
-
-    int result = Factorial(n) / (Factorial(k) * Factorial(n - k));
-
-    cout << result;
-}
+//#include <iostream>
+//using namespace std;
+//
+//int Factorial(int t) 
+//{
+//    int ans = 1;
+//    for (int i = t; i > 0; i--) {
+//        ans *= i;
+//    }
+//    return ans;
+//}
+//
+//int main() 
+//{
+//    int n, k;
+//    cin >> n >> k;
+//
+//    int result = Factorial(n) / (Factorial(k) * Factorial(n - k));
+//
+//    cout << result;
+//}
