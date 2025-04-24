dd1671733efea878714c3963bd4c87668cded0f6
ahahau <ahahau1@naver.com>
2025-04-24 20:25:02 +0900

    1312

---
+++ BackJooncpp/BackJooncpp/1312.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a, b, n;
+	cin >> a >> b >> n;
+
+	a = a % b; 
+
+	for (int i = 0; i < n - 1; i++)
+	{
+		a *= 10;
+		a %= b; 
+	}
+
+	a *= 10;
+	cout << a / b;
+}
