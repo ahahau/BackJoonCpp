2a31dd44f2d544c796b6c51e311d97ab20128b1e
ahahau <ahahau1@naver.com>
2025-04-26 23:46:24 +0900

    1402

---
+++ BackJooncpp/BackJooncpp/1402.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+//int main()
+//{
+//	int inp;
+//	cin >> inp;
+//	vector<string> arr;
+//	for (int i = 0; i < inp; i++)
+//	{
+//		int a,b,aans,bans;
+//		cin >> a >> b;
+//		int Max;
+//		Max = max(a, b);
+//		vector<int> arr1;
+//		for (int j = 1; j < Max; j++)
+//		{
+//			while (Max % j == 0)
+//			{
+//				arr1.push_back(j);
+//				Max -= j;
+//			}
+//		}
+//		for (int j : arr1)
+//		{
+//			aans *= j;
+//			bans += j;
+//		}
+//		if (a == aans && b == bans)
+//		{
+//			arr.push_back("yes");
+//		}
+//		else
+//		{
+//			arr.push_back("no");
+//		}
+//	}
+//
+//}
+int main()
+{
+	int n;
+	cin >> n;
+	while (n--)
+	{
+		cout << "yes" << endl;
+	}
+}
