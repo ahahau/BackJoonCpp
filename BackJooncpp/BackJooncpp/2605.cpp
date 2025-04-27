bb947a77ae1edc298bca52e976fd96ee8849e1d0
ahahau <ahahau1@naver.com>
2025-04-27 23:16:31 +0900

    2605

---
+++ BackJooncpp/BackJooncpp/2605.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a;
+	vector<int>arr(a);
+	for (int i = 1; i <= a; i++) {
+		cin >> b;
+		arr.insert(arr.begin() + b, i);
+	}
+	for (int i = a - 1; i >= 0; i--)
+		cout << arr[i] << ' ';
+}
