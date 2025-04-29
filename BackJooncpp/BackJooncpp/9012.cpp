3723e648cf41ae750f9bcf7fb9e547c896fd88ba
ahahau <ahahau1@naver.com>
2025-04-29 22:31:42 +0900

    9012

---
+++ BackJooncpp/BackJooncpp/9012.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int testcase;
+    cin >> testcase;
+
+    for (int i = 0; i < testcase; i++)
+    {
+        stack<char> st;
+        string s;
+        cin >> s;
+
+        for (int j = 0; j < s.length(); j++)
+        {
+            if (st.empty())
+            {
+                st.push(s[j]);
+            }
+            else
+            {
+                if (st.top() == '(' && s[j] == ')')
+                {
+                    st.pop();
+                }
+                else
+                {
+                    st.push(s[j]);
+                }
+            }
+        }
+        if (st.empty())
+        {
+            cout << "YES" << "\n";
+        }
+        else {
+            cout << "NO" << "\n";
+        }
+    }
+}
