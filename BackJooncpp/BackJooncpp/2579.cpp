95e99760fb259a6a866cf10d842f37c74e38e13b
ahahau <ahahau1@naver.com>
2025-05-23 20:51:11 +0900

    2579

--- BackJooncpp/BackJooncpp/2579.cpp
+++ BackJooncpp/BackJooncpp/2579.cpp
@@ -1,29 +1,29 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
-int Cal(int stair, int cnt,int ccnt, vector<int> arr)
-{
-	if (stair >= arr.size())
-	{
-		if (stair == arr.size() - 1)
-		{
-			cnt += arr[arr.size() - 1];
-		}
-		return cnt;
-	}
-	if (ccnt >= 2)
-		return Cal(stair + 2, cnt + arr[stair], 0, arr);
-	return max(Cal(stair + 1, cnt + arr[stair],ccnt + 1, arr), Cal(stair + 2, cnt + arr[stair],0, arr));
-}
-int main()
-{
-	int stair = 0, inp, cnt = 0;
-	cin >> inp;
-	vector<int> arr(inp);
-	for (int i = 0; i < inp; i++)
-	{
-		cin >> arr[i];
-	}
-	cout <<  max(Cal(stair, cnt,1, arr), Cal(stair + 1, cnt,0, arr));
 
+int main() {
+    int n;
+    cin >> n;
+    vector<int> stair(n + 1);
+    vector<int> dp(n + 1);
+
+    for (int i = 1; i <= n; i++) {
+        cin >> stair[i];
+    }
+
+    if (n == 1) {
+        cout << stair[1];
+        return 0;
+    }
+
+    dp[1] = stair[1];
+    dp[2] = stair[1] + stair[2];
+
+    for (int i = 3; i <= n; i++) {
+        dp[i] = max(dp[i - 2] + stair[i], dp[i - 3] + stair[i - 1] + stair[i]);
+    }
+
+    cout << dp[n];
 }
