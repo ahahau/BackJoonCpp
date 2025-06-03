8dad9934d71162446189e3330bdab3a31654be2a
ahahau <ahahau1@naver.com>
2025-06-03 23:51:47 +0900

    1350

--- BackJooncpp/BackJooncpp/1350.cpp
+++ BackJooncpp/BackJooncpp/1350.cpp
@@ -1,29 +1,27 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
+
 int main()
 {
-	int inp, cnt = 0,save;
-	cin >> inp;
-	vector<int> arr(inp);
-	for (int i = 0; i < inp; i++)
-	{
-		cin >> arr[i];
-	}
-	cin >> save;
-	for (int i = 0; i < inp; i++)
-	{
-		int fcnt = 0;
-		if (arr[i] == 0)
-			continue;
-		while (true)
-		{
-			if (arr[i] < 0)
-				break;
-			fcnt++;
-			arr[i] -= save;
-		}
-		cnt += fcnt;
-	}
-	cout << cnt * save;
+    int inp;
+    long long total = 0;
+    cin >> inp;
+    vector<int> arr(inp);
+    for (int i = 0; i < inp; i++) 
+    {
+        cin >> arr[i];
+    }
+
+    int save;
+    cin >> save;
+
+    for (int i = 0; i < inp; i++)
+    {
+        if (arr[i] == 0) continue;
+
+        total += ((long long)arr[i] + save - 1) / save;
+    }
+
+    cout << total * save ;
 }
